61eacb356a02cebe270fec9b2837031a550e45e6
RandomBinary <daniovision@yandex.com>
2015-07-24 17:00:42 +0200

    Added some shit.
    MySQL connection module, account registration script.

---
+++ incl/sql.php
@@ -0,0 +1,2 @@
+<?php
+    $link = mysqli_connect("localhost", "root", "", "sekty") or die("Failed to connect, aborting mission.");
---
+++ reg.php
@@ -0,0 +1,10 @@
+<?php
+    include_once 'incl/sql.php';
+    $username = mysqli_escape_string($link, htmlspecialchars($_POST['username']));
+    $pass = mysqli_escape_string($link, $_POST['pass']);
+    $mail = mysqli_escape_string($link, htmlspecialchars($_POST['mail']));
+    if(mysqli_num_rows(
+        mysqli_query($link, "SELECT id FROM `users` WHERE `nick`='$username'")
+    ) == 0){
+        mysqli_query($link, "INSERT INTO `users` (`nick`,`pass`,`mail`) VALUES ('$username', '$pass', '$mail')");
+    }

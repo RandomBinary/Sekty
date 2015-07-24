21d095d6784f385f32e4d6d49ffc097fce0d8336
RandomBinary <daniovision@yandex.com>
2015-07-24 16:34:20 +0200

    Seks
    Mosasaurus

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="Sex,drugs and rock'n'roll">
+
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="css.css" />
+		<title>Sekty - Załóż swoją sektę i zabij księdza.</title>
+	</head>
+	<body>
+		<h1>Witaj na Sekty.ugu.pl!</h1>
+		<h2>
+			<form action="login.php" method="post">
+			    
+			    Nick: <input name="username" type="text" />
+			    <br/>
+			    Hasło: <input name="pass" type="password" />
+			    <br/>
+			    <input type="submit" value="Zaloguj"
+			</form>
+		</h2>
+	</body>
+
+</html>

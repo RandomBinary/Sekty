3770b616f07bf01a6b96f61e9249525f093d7964
RandomBinary <daniovision@yandex.com>
2015-07-24 21:11:30 +0200

    Here comes baby boo-boo.
    Added descriptions, fixed formatting.

--- incl/topbar.php
+++ incl/topbar.php
@@ -1,18 +1,21 @@
 <table border="0" class="topbar">
-            <tbody><tr>
-                <Td class="user">
-                    <table>
-                        <td>
-                            <?php echo "<img src=\"" . $user['avatar'] . "\" alt=\"Twój avatar.\" class=\"avatar\">"; ?>
-                        </td>
-                        <Td>
-                            <?php echo "<strong>" . $user['nick'] . "</strong>"; ?><br>
-                            Cycki: 5
-                        </Td>
-                    </table>
+    <tbody><tr>
+        <!-- Informacje o użytkowniku -->
+        <td class="user">
+            <table>
+                <td>
+                    <?php echo "<img src=\"" . $user['avatar'] . "\" alt=\"Twój avatar.\" class=\"avatar\">"; ?>
+                </td>
+                <Td>
+                    <?php echo "<strong>" . $user['nick'] . "</strong>"; ?><br>
+                    Cycki: 5
                 </Td>
-                <Td class="menu">
-                    <a href="logout.php"><img src="img/logout.gif" alt="Wyloguj się"></a>
-                </Td>
-            </tr></tbody>
-        </table>
+            </table>
+        </td>
+        
+        <!-- Menu -->
+        <td class="menu">
+            <a href="logout.php"><img src="img/logout.gif" alt="Wyloguj się"></a>
+        </Td>
+    </tr></tbody>
+</table>
